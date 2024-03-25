795836e3639409e1e37632a6e0b57cf15b8f85ff
Justin Moore <Kaemin@gmail.com>
2024-03-24 21:44:16 -0500

    Update CreateNewADUserAccount.ps1

--- CreateNewADUserAccount.ps1
+++ CreateNewADUserAccount.ps1
@@ -3,7 +3,7 @@
 Import-Module activedirectory
   
 #Store the data from ADUsers.csv in the $ADUsers variable
-$ADUsers = Import-csv C:\Users\Terry.Joseph\Desktop\bulk_users1.csv
+$ADUsers = Import-csv C:\Users\Justin.Moore\Desktop\bulk_users1.csv
 
 #Loop through each row containing user details in the CSV file 
 foreach ($User in $ADUsers)
